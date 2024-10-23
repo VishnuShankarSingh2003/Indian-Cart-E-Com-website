b3bf6657b2247b4b6dc1a6a51b6fe0f38648dd8e
VISHNU SHANKAR SINGH <104004983+VishnuShankarSingh2003@users.noreply.github.com>
2024-10-24 04:49:14 +0530

    Update ShopContext.jsx

--- e-commerce-frontend/src/Context/ShopContext.jsx
+++ e-commerce-frontend/src/Context/ShopContext.jsx
@@ -16,7 +16,7 @@ const ShopContextProvider = (props) => {
   const [cartItems, setCartItems] = useState(getDefaultCart());
 
   useEffect(() => {
-    fetch("http://localhost:4000/allproducts")
+    fetch("https://indian-cart-e-com-website.onrender.com/allproducts")
       .then((res) => res.json())
       .then((data) => {
         // console.log("data "+data);
@@ -24,7 +24,7 @@ const ShopContextProvider = (props) => {
       });
 
     if (localStorage.getItem("auth-token")) {
-      fetch("http://localhost:4000/getcart", {
+      fetch("https://indian-cart-e-com-website.onrender.com/getcart", {
         method: "POST",
         headers: {
           Accept: "application/form-data",
@@ -68,7 +68,7 @@ const ShopContextProvider = (props) => {
   const addToCart = (itemId) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     if (localStorage.getItem("auth-token")) {
-      fetch("http://localhost:4000/addtocart", {
+      fetch("https://indian-cart-e-com-website.onrender.com/addtocart", {
         method: "POST",
         headers: {
           Accept: "application/form-data",
@@ -87,7 +87,7 @@ const ShopContextProvider = (props) => {
   const removeFromCart = (itemId) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
     if (localStorage.getItem("auth-token")) {
-      fetch("http://localhost:4000/removefromcart", {
+      fetch("https://indian-cart-e-com-website.onrender.com/removefromcart", {
         method: "POST",
         headers: {
           Accept: "application/form-data",
